c62dc5cd3c60c7c8c76645523e2f87eecc77ed4a
siraht <t@travishinton.us>
2021-12-02 18:45:35 -0700

    Tons of refactoring, functionality is 99% done, and lots of CSS improvements

--- main-copy.js
+++ main-copy.js
@@ -7,27 +7,59 @@ const nameTwoInput = body.querySelector('.nameTwoInput')
 const restartButton = body.querySelector('.restartButton')
 const congrats = body.querySelector('.congratulationsMessage')
 const gameBoardContainer = body.querySelector('.gameBoardContainer')
+const playOrderButton = body.querySelector('.playOrderButton')
+const leftContainer = body.querySelector('.leftContainer')
 
+/* let playerOne;
+let playerTwo; */
+playerChoice = 'X';
 
 // Gameboard Object and Arrays
 let gameBoard = (function () {
 
-    // Marker Listener Function Expression
+    let gameBoardArray = ['', '', '', '', '', '', '', '', '']
+
+
+    // Play order functionality
+    let whosTurn;
+    let playOrderListener = (item) => {
+        if (playOrderButton.textContent == "Who goes first?") {
+            playOrderButton.textContent = `${playerOne.name}`;
+            whosTurn = playerOne;
+        } else if (playOrderButton.textContent == `${playerOne.name}`) {
+            playOrderButton.textContent = `${playerTwo.name}`;
+            whosTurn = playerTwo;
+        } else {
+            playOrderButton.textContent = `${playerOne.name}`;
+            whosTurn = playerOne;
+        }
+    }
+    playOrderButton.addEventListener('click', playOrderListener);
+
+
+    // Marker placement function expression
     let addMarkerListener = (item) => {
         item.addEventListener('click', event => {
             if (item.textContent || gameOver == true) {
 
             } else {
-                item.textContent = playerOneMarkerButton.textContent;
-                gameBoardArray[item.id] = playerOneMarkerButton.textContent;
                 gameStarted = true;
-                checkGameEnd();
+                if (whosTurn == playerOne) {
+                    item.textContent = playerOneMarkerButton.textContent;
+                    gameBoardArray[item.id] = playerOneMarkerButton.textContent;
+                    checkGameEnd();
+                    whosTurn = playerTwo;
+                } else {
+                    item.textContent = playerTwoMarkerButton.textContent;
+                    gameBoardArray[item.id] = playerTwoMarkerButton.textContent;
+                    checkGameEnd();
+                    whosTurn = playerOne;
+                }
+
             }
 
         })
     }
-
-    let gameBoardArray = ['', '', '', '', '', '', '', '', '']
 
     // New Board Square Function Expression
     let newBoardSquare = function (index) {
@@ -45,15 +77,34 @@ let gameBoard = (function () {
         }
         document.querySelectorAll('.boardSquare').forEach(addMarkerListener)
     })();
+
     let gameOver;
+
+
     let checkGameEnd = () => {
-        if ((gameBoardArray[0] == 'O' && gameBoardArray[1] == 'O' && gameBoardArray[2] == 'O') || (gameBoardArray[0] == 'O' && gameBoardArray[3] == 'O' && gameBoardArray[6] == 'O') || (gameBoardArray[0] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[8] == 'O') || (gameBoardArray[6] == 'O' && gameBoardArray[7] == 'O' && gameBoardArray[8] == 'O') || (gameBoardArray[2] == 'O' && gameBoardArray[5] == 'O' && gameBoardArray[8] == 'O') || (gameBoardArray[1] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[7] == 'O') || (gameBoardArray[3] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[5] == 'O')) {
+        if ((gameBoardArray[0] == 'O' && gameBoardArray[3] == 'O' && gameBoardArray[6] == 'O') || (gameBoardArray[1] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[7] == 'O') || (gameBoardArray[2] == 'O' && gameBoardArray[5] == 'O' && gameBoardArray[8] == 'O') || (gameBoardArray[0] == 'O' && gameBoardArray[1] == 'O' && gameBoardArray[2] == 'O') || (gameBoardArray[3] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[5] == 'O') || (gameBoardArray[6] == 'O' && gameBoardArray[7] == 'O' && gameBoardArray[8] == 'O') || (gameBoardArray[0] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[8] == 'O') || (gameBoardArray[2] == 'O' && gameBoardArray[4] == 'O' && gameBoardArray[6] == 'O')) {
             gameOver = true;
-            alert(`O Wins!`);
-        } else if ((gameBoardArray[0] == 'X' && gameBoardArray[1] == 'X' && gameBoardArray[2] == 'X') || (gameBoardArray[0] == 'X' && gameBoardArray[3] == 'X' && gameBoardArray[6] == 'X') || (gameBoardArray[0] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[8] == 'X') || (gameBoardArray[6] == 'X' && gameBoardArray[7] == 'X' && gameBoardArray[8] == 'X') || (gameBoardArray[2] == 'X' && gameBoardArray[5] == 'X' && gameBoardArray[8] == 'X') || (gameBoardArray[1] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[7] == 'X') || (gameBoardArray[3] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[5] == 'X')) {
+            if (playerOne.marker == 'O') {
+                congratulationsMessage(`${playerOne.name}`)
+            } else if (playerTwo.marker == 'O') {
+                congratulationsMessage(`${playerTwo.name}`)
+            }
+        } else if ((gameBoardArray[0] == 'X' && gameBoardArray[3] == 'X' && gameBoardArray[6] == 'X') || (gameBoardArray[1] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[7] == 'X') || (gameBoardArray[2] == 'X' && gameBoardArray[5] == 'X' && gameBoardArray[8] == 'X') || (gameBoardArray[0] == 'X' && gameBoardArray[1] == 'X' && gameBoardArray[2] == 'X') || (gameBoardArray[3] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[5] == 'X') || (gameBoardArray[6] == 'X' && gameBoardArray[7] == 'X' && gameBoardArray[8] == 'X') || (gameBoardArray[0] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[8] == 'X') || (gameBoardArray[2] == 'X' && gameBoardArray[4] == 'X' && gameBoardArray[6] == 'X')) {
             gameOver = true;
-            alert(`X Wins!`);
+            if (playerOne.marker == 'X') {
+                congratulationsMessage(`${playerOne.name}`)
+            } else if (playerTwo.marker == 'X') {
+                congratulationsMessage(`${playerTwo.name}`)
+            }
         }
+    }
+
+    let congratulationsMessage = (winner) => {
+        newDiv = document.createElement('h2');
+        newDiv.id = `Congratulations`
+        newDiv.textContent = `${winner} wins!`
+        newDiv.classList.add('congratulationsMessage');
+        leftContainer.appendChild(newDiv);
     }
 
     // Restart functionality
@@ -61,8 +112,16 @@ let gameBoard = (function () {
     let resetGame = () => {
         gameBoardArray = ['', '', '', '', '', '', '', '', '']
         allBoardSquares.forEach(element => element.textContent = '');
-        gameOver = false;
+        nameOneInput.value = 'Player One'
+        nameTwoInput.value = 'Player Two'
         gameStarted = false;
+        gameOver = false;
+        playerOneName = nameOneInput.value;
+        playerOneChoice = playerOneMarkerButton.textContent;
+        playerOne = players(playerOneName, playerOneChoice);
+        playerTwoName = nameTwoInput.value;
+        playerTwoChoice = playerTwoMarkerButton.textContent;
+        playerTwo = players(playerTwoName, playerTwoChoice);
     }
     restartButton.addEventListener('click', resetGame);
 
@@ -90,10 +149,10 @@ let gameBoard = (function () {
                 body.querySelectorAll('.markerButton').forEach(element => element.classList.toggle('X'));
                 if (event.target.classList.contains('X')) {
                     event.target.textContent = 'X'
-                    playerTwoMarkerButton.textContent = 'O'
+                    playerOneMarkerButton.textContent = 'O'
                 } else {
                     event.target.textContent = 'O'
-                    playerTwoMarkerButton.textContent = 'X'
+                    playerOneMarkerButton.textContent = 'X'
                 }
             }
         }
@@ -117,11 +176,6 @@ const players = (name, marker) => {
     return { name, marker, wins, loses };
 };
 
-let playerOne;
-let playerTwo
-playerChoice = 'X';
-let npc;
-
 // Refresh Player Name and Marker
 let refreshPlayer = () => {
     playerOneName = nameOneInput.value;
@@ -131,6 +185,14 @@ let refreshPlayer = () => {
     playerTwoChoice = playerTwoMarkerButton.textContent;
     playerTwo = players(playerTwoName, playerTwoChoice);
 }
+
+let initialize = (function () {
+    nameOneInput.value = 'Player One'
+    nameTwoInput.value = 'Player Two'
+    gameStarted = false;
+    gameOver = false;
+    refreshPlayer()
+})();
 
 nameOneInput.addEventListener('input', refreshPlayer);
 nameTwoInput.addEventListener('input', refreshPlayer);
